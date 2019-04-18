50802e924da9465d4888d0291731638773affe57
zzdaw <hohi@wp.pl>
2019-04-18 12:52:45 +0200

    Updates

---
+++ precache-manifest.09327e2e3be45d15e3b42b580db2a2de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c50e0eb0bc0414026c30",
+    "url": "/todo-redux/static/js/runtime~main.d559cabe.js"
+  },
+  {
+    "revision": "0db49db5c1b4282d9a1e",
+    "url": "/todo-redux/static/js/main.cb640491.chunk.js"
+  },
+  {
+    "revision": "dcfcb913d5b2302d9588",
+    "url": "/todo-redux/static/js/2.7701d0bc.chunk.js"
+  },
+  {
+    "revision": "0db49db5c1b4282d9a1e",
+    "url": "/todo-redux/static/css/main.9a5fe95d.chunk.css"
+  },
+  {
+    "revision": "6cb4220a58ed00572c5a817683d868cb",
+    "url": "/todo-redux/index.html"
+  }
+];
